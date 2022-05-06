79402cd76990aec06101e14a513571208f787307
Luke Marshall <luke.marshall.03@gmail.com>
2022-05-06 08:27:40 -0400

    Changed python to version 3 and added brackets to madlib variable

--- Project-1-madlib/madlibs.py
+++ Project-1-madlib/madlibs.py
@@ -7,7 +7,7 @@ verb1 = input("Verb1: ")
 verb2 = input("Verb2: ")
 famous_person = input("Famous Person: ")
 
-madlib = f"Python programming is really {adj}! It makes me so excited all the time because \
-I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!"
+madlib = (f"Python programming is really {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!")
 
 print(madlib)
