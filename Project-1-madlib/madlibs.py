5cfd76896bce093b3287316fb8c63def6550bde5
Luke Marshall <luke.marshall.03@gmail.com>
2022-05-06 08:03:00 -0400

    created new python madlibs code

---
+++ Project-1-madlib/madlibs.py
@@ -0,0 +1,13 @@
+# madlibs program that uses f string.
+# User inputs adjectives and verbs to complete sentence.
+
+
+adj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb2: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Python programming is really {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!"
+
+print(madlib)
