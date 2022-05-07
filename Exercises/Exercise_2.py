e3366f1ae8d8c2496f2c7b19604f6571073ca516
Luke Marshall <luke.marshall.03@gmail.com>
2022-05-07 10:09:02 -0400

    Fixed Exercise 2. Removed previous_num and added i in range.

--- Exercises/Exercise_2.py
+++ Exercises/Exercise_2.py
@@ -2,11 +2,13 @@
 # STEPS
 # 1. Create a variable called previous_num and assign it to 0
 # 2. Iterate the first 10 numbers one by one using for loop and range() function
+# 3. Next, display the current number (i), previous number, and the addition of both numbers in each iteration of the loop.
+#    At last, change the value previous number to current number (previous_num = i)
 
 previous_num = 0
 
 print('Printing current and previous number sum in a range(10)')
 
-# This is partially working. issue when printing previous number and sum during first iteration. Both -1
-for previous_num in range(0, 10):
-    print(f'Current Number {previous_num} Previous Number {previous_num - 1} Sum: {previous_num + previous_num -1} ')
+for i in range(10):
+    print(f'Current Number {previous_num} Previous Number {i} Sum: {previous_num + i} ')
+    previous_num = i
