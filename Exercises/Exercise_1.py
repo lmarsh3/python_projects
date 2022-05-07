a9ba0c56e562930d3150d9dc3d11864ad7f9c550
Luke Marshall <luke.marshall.03@gmail.com>
2022-05-07 08:06:30 -0400

    Added Exercise 1

---
+++ Exercises/Exercise_1.py
@@ -0,0 +1,9 @@
+# Exercise # 1 - Calculate the multiplication and sum of two numbers
+
+number1 = int(input('Please enter 1st number: '))
+number2 = int(input('Please enter 2nd number: '))
+
+if (number1 * number2) > 1000: # Check to see if the product of 2 numbers is greater than 1000
+    print(f'The result is: {number1 * number2}')
+else:
+    print(f'The result is: {number1 + number2}')
